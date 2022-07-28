429b0d7297075fb4d164a4af322a17a24892ee83
Akinbinu Arike <akinbinuarike2020@gmail.com>
2022-07-27 18:10:20 -0700

    first commit

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const header = document.querySelector('.navbar');
 
 window.onscroll = function() {
-    var top = window.scrollY;
+    const top = window.scrollY;
     if(top >=100) {
         header.classList.add('navbarDark');
     }
@@ -9,3 +9,21 @@ window.onscroll = function() {
         header.classList.remove('navbarDark');
     }
 }
+
+
+
+// function openCity(evt, cityName) {
+//   var i, tabcontent, tablinks;
+//   tabcontent = document.getElementsByClassName("tabcontent");
+//   for (i = 0; i < tabcontent.length; i++) {
+//     tabcontent[i].style.display = "none";
+//   }
+//   tablinks = document.getElementsByClassName("tablinks");
+//   for (i = 0; i < tablinks.length; i++) {
+//     tablinks[i].className = tablinks[i].className.replace(" active", "");
+//   }
+//   document.getElementById(cityName).style.display = "block";
+//   evt.currentTarget.className += " active";
+// }
+
+   
